efd56ed069a15572da386842e857d1131dcd4ebf
Vanessa <vanessa.y.cui@gmail.com>
2022-07-14 17:28:39 -0400

    added  buttons to link between recipes and item screen views. added launch screen

--- always-fresh/Controller/ViewController.swift
+++ always-fresh/Controller/ViewController.swift
@@ -7,13 +7,26 @@
 
 import UIKit
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController {
 
+    @IBOutlet weak var goToItems: UIButton!
+    @IBOutlet weak var goToRecipes: UIButton!
+    @IBOutlet weak var goToScanner: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
-
-
+    
+    @IBAction func goToItems(_ sender: UIButton) {
+        
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    @IBAction func goToRecipes(_ sender: UIButton) {
+        performSegue(withIdentifier: "goToRecipes", sender: self)
+    }
 }
 
